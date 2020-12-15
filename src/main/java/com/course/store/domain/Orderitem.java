d1e5f549aefafd94e2a5f5b3731f893c7cf3d805
tusake <61720409+tusake@users.noreply.github.com>
2020-12-15 21:47:37 +0800

    Add files via upload

---
+++ src/main/java/com/course/store/domain/Orderitem.java
@@ -0,0 +1,50 @@
+package com.icss.mystore.domain;
+
+public class Orderitem {
+    private int count;
+    private double subtotal;
+    private String pid;
+    private String oid;
+    public Orderitem(){
+
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public Orderitem(int count, double subtotal, String pid, String oid) {
+        this.count = count;
+        this.subtotal = subtotal;
+        this.pid = pid;
+        this.oid = oid;
+    }
+}
---
+++ src/main/java/com/course/store/domain/Orders.java
@@ -0,0 +1,92 @@
+package com.icss.mystore.domain;
+
+public class Orders {
+    private String oid;
+    private String ordertime;
+    private double total;
+    private int state;
+    private String address;
+    private String name;
+    private String telephone;
+    private String uid;
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getOrdertime() {
+        return ordertime;
+    }
+
+    public void setOrdertime(String ordertime) {
+        this.ordertime = ordertime;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public Orders() {
+
+    }
+
+    public Orders(String oid, String ordertime, double total, int state, String address,
+                  String name, String telephone, String uid) {
+        this.oid = oid;
+        this.ordertime = ordertime;
+        this.total = total;
+        this.state = state;
+        this.address = address;
+        this.name = name;
+        this.telephone = telephone;
+        this.uid = uid;
+    }
+}
