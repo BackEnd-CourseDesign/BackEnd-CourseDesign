9a31b64afbea76f7b26fb095eab275eda6312050
yuqing521 <1581446178@qq.com>
2020-12-15 12:06:50 +0800

    update domain entity

---
+++ src/main/java/com/course/store/domain/Orders.java
@@ -0,0 +1,91 @@
+package com.icss.mystore.domain;
+
+public class Orders {
+    private String oid;
+    private String ordertime;
+    private String total;
+    private String state;
+    private String address;
+    private String name;
+    private String telephone;
+    private String uid;
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getOrdertime() {
+        return ordertime;
+    }
+
+    public void setOrdertime(String ordertime) {
+        this.ordertime = ordertime;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public Orders(){
+
+    }
+
+    public Orders(String oid, String ordertime, String total, String state, String address, String name, String telephone, String uid) {
+        this.oid = oid;
+        this.ordertime = ordertime;
+        this.total = total;
+        this.state = state;
+        this.address = address;
+        this.name = name;
+        this.telephone = telephone;
+        this.uid = uid;
+    }
+}
